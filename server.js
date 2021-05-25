7029e5a0d57791de5f9be06bfab6d88e7e9f1907
Caitlin <>
2021-05-25 16:55:36 +0000

    API that parses timestamp and returns JSON

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// server.js
-// where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
@@ -29,4 +26,26 @@ app.get("/api/hello", function (req, res) {
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get("/api", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});
+
+
+app.get("/api/:date", function(req, res) {
+  let date = req.params.date;
+
+  if (/\d{5,}/.test(date)) {
+    dateInt = parseInt(date);
+    res.json({"unix": dateInt, "utc": new Date(dateInt).toUTCString()});
+  };
+
+  let date_test = new Date(date);
+
+  if (date_test.toString() == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: date_test.valueOf(), utc: date_test.toUTCString() });
+    }
 });
